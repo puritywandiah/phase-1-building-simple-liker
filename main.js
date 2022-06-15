874004ef3bfb8b8c5777e0833824f5128afed73d
+ <+>
2022-06-16 01:50:45 +0300

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const heart = document.querySelectorAll(".like-glyph")
+heart.forEach( (element) => {
+  
+  element.addEventListener("click", () => {
 
+    mimicServerCall()
+    .then(() => {
+
+      if(element.innerHTML === FULL_HEART) {
+        element.innerHTML = EMPTY_HEART
+        element.classList.remove("activated-heart")
+      }else {
+        element.innerHTML = FULL_HEART
+        element.classList = "activated-heart"
+      }
+
+    })
+  
+    .catch(error => {
+      const message = document.querySelector("#modal")
+      message.classList.remove("hidden")
+  
+      setTimeout( () => {
+        message.classList = "hidden"
+      }, 3000)
+    })
+  })
+
+})
 
 
 //------------------------------------------------------------------------------
